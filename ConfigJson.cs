7855019a87e9cf101e67e1a75adc4de803aec503
Serana Wilger <sbwilger@gmail.com>
2020-08-31 01:38:41 -0400

    Add files via upload

---
+++ ConfigJson.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+namespace BirdyBotv2
+{
+    //struct to store data from the Json
+    public struct ConfigJson
+    {
+        [JsonProperty("token")]
+        public string Token { get; private set; }
+        [JsonProperty("prefix")]
+        public string Prefix { get; private set; }
+    }
+}
