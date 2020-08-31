f74f5f5806ab0532d52927f00a599f62710228f4
Serana Wilger <sbwilger@gmail.com>
2020-08-31 01:38:50 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BirdyBotv2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bot bot = new Bot();
+            bot.RunAsync().GetAwaiter().GetResult();
+        }
+    }
+}
